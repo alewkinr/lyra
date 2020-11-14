a916c3b6fedce91ce9c8674f9a0fc77500151d0c
Ramil Aleshkin <alewkin@icloud.com>
2020-11-14 05:39:17 +0300

    :sparkles: Added service skeleton generator
    - generated model service

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 DOCKER_REGISTRY="docker.pkg.github.com/alewkinr/lyra/"
+SERVICE_NAME?=service
 
+generate:
+	grep -r -l '{{SERVICE_NAME}}' . | grep -v "Makefile" | xargs sed -i -e 's/{{SERVICE_NAME}}/$(SERVICE_NAME)/g'
+	find . -name '*.*-e' -exec rm {} +
 freeze:
 	pip freeze > requirements.txt
 
 require:
 	pip install -r requirements.txt
-
-generate:
-	cookiecutter https://github.com/tiangolo/full-stack-fastapi-postgresql
---
+++ back/model/model/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from model.api.v1 import v1
+from model.core.config import settings
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(v1, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+    # TODO: fix before release
+    uvicorn.run(app, host="0.0.0.0", port=8081)
